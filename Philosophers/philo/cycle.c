82d73d9ae00f2450af0f1128c50bd0e32dc797fd
yongjale <yongjale@student.42.fr>
2023-07-22 23:38:23 +0900

    updated

--- Philosophers/philo/cycle.c
+++ Philosophers/philo/cycle.c
@@ -24,8 +24,29 @@ int	init_cycle(t_ph *ph)
 		while (num < ph->num_philo)
 		{
 			if (num % 2 == turn && !(turn && num == ph->num_philo - 1)) // 홀수 & 짝수 구분 (홀수이면서 마지막 값은 안됨)
-				pthread_mutex_lock(&forks[num]);
-			
+				get_eat(ph, num);
+			get_sleep(ph, turn);
 		}
+	}
+}
+
+int	get_eat(t_ph* ph, int num)
+{
+	phread_mutex_lock(&ph->forks[num]);
+	if (num == ph->num_philo)
+		pthread_mutex_locK(&ph->forks[0]);
+	else
+		pthread_mutex_lock(&ph->forks[num + 1]);
+	printf("")
+}
+
+void	get_sleep(t_ph* ph, int turn)
+{
+	int	num;
+
+	num = 0;
+	while (num < ph->num_philo)
+	{
+		
 	}
 }
